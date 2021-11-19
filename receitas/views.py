30d3c85df0ded94f71a225af06d56dd99ede13d7
hashtegui <guilherme.castro.souza1999@gmail.com>
2021-11-19 17:36:53 -0400

    atualizacao de templates

--- receitas/views.py
+++ receitas/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Receitas</h1><h2>BEM VINDO</h2>')
+    return render(request,'index.html')
